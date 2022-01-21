4246cdfe203d0496795b275a66fa2355c1173d21
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-21 19:23:33 +0300

    Check whether name or surname is a part of username

--- regex_op.py
+++ regex_op.py
@@ -54,10 +54,15 @@ class RegexOperations:
     def is_valid_username_email(self,username:str,mail:str)->bool:
         pass
     
-    #TODO 2
     def is_valid_namesurname_username(self,name:str,surname:str,username:str)->bool:
-        pass
-
+        # create substring with name & surname
+        sub_name=[name[i:i+4] for i in range(len(name)-4)]
+        sub_surname=[surname[i:i+4] for i in range(len(surname)-4)]
+        # check any part of substring is passing in username
+        if (any([True if item in username else False for item in sub_name])) or (any([True if item in username else False for item in sub_surname])):
+            return True
+        else:
+            return False
     #TODO 3
     def is_valid_birth_year(self,birth_year:str)->bool:
         pass
@@ -72,4 +77,5 @@ class RegexOperations:
 
 if __name__=='__main__':
     regex_obj=RegexOperations()
-    regex_obj.is__valid_name_surname(name='hasanasdagmaaaxxxxxa',surname='ozdemir')
+    # regex_obj.is__valid_name_surname(name='hasanasdagmaaaxxxxxa',surname='ozdemir')
+    # regex_obj.is_valid_namesurname_username('hasan','ozdemir','cancancan')
