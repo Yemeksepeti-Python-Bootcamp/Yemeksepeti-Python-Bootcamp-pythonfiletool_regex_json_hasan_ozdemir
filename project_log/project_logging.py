a3bb2134751b94b59b05f571122f2a6a19f866d8
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-21 23:44:06 +0300

    Logging have added. Details TBD

---
+++ project_log/project_logging.py
@@ -0,0 +1,24 @@
+
+from cmath import log
+import logging
+
+#TODO all methods content TBD
+class ProjectLogging:
+
+    def __init__(self) -> None:
+        pass
+
+    def successful_log(self)-> None:
+        pass
+
+    def value_error_log(self)->None:
+        pass
+
+    def missing_arg_log(self)-> None:
+        pass
+
+    def file_not_found_log(self)->None:
+        pass
+    
+if __name__=='__main__':
+    log_obj=ProjectLogging()
