bfce0e81f92d8ee98e18c9ed912a3b910bef9629
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-21 23:44:50 +0300

    Test

--- regex/helper.py
+++ regex/helper.py
@@ -4,19 +4,35 @@ This script is created to manage helper methods
 """
 
 from argparse import ArgumentParser
-from sys import argv
+from sys import argv,path
+
 
 class HelperRegex:
 
-    def is_leap_year(self,year:int)->bool:
+    def is_leap_year(self,year:str)->bool:
         """
         This method is created to check year whether is leap nor not leap
         :param year: <int> year
         :return: <bool> Leap  or Not Leap
         """
-        if (year%400==0) or ((year%100==0) and (year%4==0)):
-            return True
+        try:
+            year=int(year)
+            if (year%400==0) or ((year%100==0) and (year%4==0)) and year:
+                return True
+            else:
+                return False
+        except ValueError as error:
+            #TODO value error log tbd
+            pass
 
-        else:
-            return False
 
+
+if __name__=='__main__':
+    try:
+        # object initialization
+        regex_obj=HelperRegex()
+        # method calling
+        regex_obj.is_leap_year('100')
+    except Exception as error:
+        #TODO missing argument log tbd
+        pass
