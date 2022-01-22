7fc6078313bd10f9284969fab432b43eb350d4c4
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-23 02:11:38 +0300

    file not found error logging method is created

--- dir_logging/project_logging.py
+++ dir_logging/project_logging.py
@@ -1,6 +1,8 @@
 
-from cmath import log
+import os
 import logging
+import logging.handlers
+
 
 #TODO all methods content TBD
 class ProjectLogging:
@@ -17,8 +19,15 @@ class ProjectLogging:
     def missing_arg_log(self)-> None:
         pass
 
-    def file_not_found_log(self)->None:
-        pass
+    def file_not_found_log(self,error_msg:str)->logging.getLogger():
+        log_format="%(levelname)s %(asctime)s -> %(message)s"
+        logging.basicConfig(filename="../project_logs.log",
+                            filemode="w",
+                            format=log_format,
+                            level=logging.ERROR)
+
+        logger_obj = logging.getLogger()
+        return logger_obj.error(error_msg)
     
 if __name__=='__main__':
     log_obj=ProjectLogging()
