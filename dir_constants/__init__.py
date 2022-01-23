f68072d0a0386bdffb29400e36f68cdfffc53bf1
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-23 03:39:39 +0300

    Directories is updated

---
+++ dir_constants/__init__.py
@@ -0,0 +1,7 @@
+"""
+This script is created to manage file paths for individual testing
+"""
+
+JSON_PATH:str= "/dir_data/dataregex.json"
+LOG_PATH:str= "/dir_data/project_logs.log"
+DB_PATH:str= "/dir_data/datagrex.db"
