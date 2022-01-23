ddcc667a8c13ce06b5740cf8e82a67e42a93e4f1
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-23 03:49:09 +0300

    Error handling connected with logging class

--- dir_regex/helper.py
+++ dir_regex/helper.py
@@ -5,9 +5,10 @@ This script is created to manage helper methods
 
 from argparse import ArgumentParser
 from sys import argv,path
+from dir_constants.project_constants import LOG_PATH
+from dir_logging.project_logging import ProjectLogging
 
-
-class HelperRegex:
+class HelperRegex(ProjectLogging):
 
     def is_leap_year(self,year:str)->bool:
         """
@@ -24,8 +25,7 @@ class HelperRegex:
             else:
                 return False
         except ValueError as error:
-            #TODO value error log tbd
-            pass
+            self.error_log(error_msg=str(error))
 
 
 
@@ -36,5 +36,5 @@ if __name__=='__main__':
         # method calling
         regex_obj.is_leap_year('100')
     except Exception as error:
-        #TODO missing argument log tbd
-        pass
+        log_obj=ProjectLogging(LOG_PATH)
+        log_obj.error_log(error_msg=str(error))
